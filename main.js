2d6fe7ddcdaf94b53e710f52ac88a164d83e8e00
Badr-21 <87186226+Badr-21@users.noreply.github.com>
2022-12-06 00:00:21 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,27 +1,22 @@
-import data from './data.json' assert { type: 'json'}
+fetch("./data.json")
+.then(response => {
+    return response.json()
+})
+.then(data => {
+    const days = document.querySelectorAll(".day");
+    const daySpent = document.querySelectorAll(".day-spent");
+    const dayName = new Date();
 
-const days = document.querySelectorAll(".day");
-const daySpent = document.querySelectorAll(".day-spent");
-const dayName = new Date();
-console.log(dayName.getUTCDay() - 1)
+    days.forEach((day, index) => {   
+        day.style.height = `${data[index].amount * 11.5 / 100}rem`;
+        if(index === dayName.getUTCDay() - 1) {
+            day.style.backgroundColor = "var(--Cyan)"
+        }
+    });
 
-days.forEach((day, index) => {   
-    day.style.height = `${data[index].amount * 11.5 / 100}rem`;
-    if(index === dayName.getUTCDay() - 1) {
-        day.style.backgroundColor = "var(--Cyan)"
-    }
+    daySpent.forEach((day, index) => {   
+        day.innerText= `$${data[index].amount}`
+    });
 });
 
-daySpent.forEach((day, index) => {   
-    day.innerText= `$${data[index].amount}`
-});
 
-
-// days.forEach(day => {   
-//     day.addEventListener("mouseover", function(e)  {
-//         day.previousSibling.classList = "active"
-//     })
-// })
-
-
-console.log(daySpent)

d9462c6ba106667177bcefc8774049d5d70771eb
Badr-21 <87186226+Badr-21@users.noreply.github.com>
2022-12-05 23:17:15 +0100

    Add files via upload
    1st commit

---
+++ main.js
@@ -0,0 +1,27 @@
+import data from './data.json' assert { type: 'json'}
+
+const days = document.querySelectorAll(".day");
+const daySpent = document.querySelectorAll(".day-spent");
+const dayName = new Date();
+console.log(dayName.getUTCDay() - 1)
+
+days.forEach((day, index) => {   
+    day.style.height = `${data[index].amount * 11.5 / 100}rem`;
+    if(index === dayName.getUTCDay() - 1) {
+        day.style.backgroundColor = "var(--Cyan)"
+    }
+});
+
+daySpent.forEach((day, index) => {   
+    day.innerText= `$${data[index].amount}`
+});
+
+
+// days.forEach(day => {   
+//     day.addEventListener("mouseover", function(e)  {
+//         day.previousSibling.classList = "active"
+//     })
+// })
+
+
+console.log(daySpent)
